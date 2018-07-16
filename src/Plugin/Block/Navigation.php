0e25d5d6560d6b23bfa62b7072f6d0d066d6e7a1
Johanna <JojoVes@users.noreply.github.com>
2018-07-16 17:26:10 -0300

    fixes dependency injection derp
    Forgot that $this->config is not available in BlockBase.

--- src/Plugin/Block/Navigation.php
+++ src/Plugin/Block/Navigation.php
@@ -5,6 +5,9 @@ namespace Drupal\islandora_compound_object\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Config\ImmutableConfig;
 
 /**
  * Provides a block for navigating a compound object's children.
@@ -14,7 +17,29 @@ use Drupal\Core\Session\AccountInterface;
  *   admin_label = @Translation("Islandora Compound Object Navigation"),
  * )
  */
-class Navigation extends BlockBase {
+class Navigation extends BlockBase implements ContainerFactoryPluginInterface {
+
+  protected $config;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')->get('islandora_compound_object.settings')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ImmutableConfig $config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->config = $config;
+  }
 
   /**
    * {@inheritdoc}
@@ -31,7 +56,7 @@ class Navigation extends BlockBase {
    * {@inheritdoc}
    */
   public function blockAccess(AccountInterface $account) {
-    if (!$this->config('islandora_compound_object.settings')->get('islandora_compound_object_use_jail_view')) {
+    if (!$this->config->get('islandora_compound_object_use_jail_view')) {
       return AccessResult::allowed();
     }
     else {
