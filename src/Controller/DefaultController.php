f8d8aeb092bf1ef3e946951b1411f8c1bcf79fa2
Johanna <JojoVes@users.noreply.github.com>
2018-07-30 18:05:22 -0300

    fix type derp

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -90,7 +90,7 @@ EOQ;
   /**
    * Access callback for tabs that aren't tabs.
    */
-  public static function islandoraCompoundObjectTaskAccess(AbstractObject $object, AccountInterface $account) {
+  public static function islandoraCompoundObjectTaskAccess($object, AccountInterface $account) {
     $object = islandora_object_load($object);
     $perm = islandora_compound_object_task_access($object);
     return ($perm && \Drupal::routeMatch()->getRouteName() == 'islandora.view_object') ? AccessResult::allowed() : AccessResult::forbidden();
@@ -99,7 +99,7 @@ EOQ;
   /**
    * Access callback for compound object management.
    */
-  public static function islandoraCompoundObjectAccess(AbstractObject $object) {
+  public static function islandoraCompoundObjectAccess($object) {
     $object = islandora_object_load($object);
     $perm = islandora_compound_object_access($object);
     return $perm ? AccessResult::allowed() : AccessResult::forbidden();
